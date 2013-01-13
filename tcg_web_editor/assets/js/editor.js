40a8f319dd4f2aadcbffe7a1dfaf45b0403c4a1f
Petr Viktorin <encukou@gmail.com>
2013-01-13 18:15:06 +0100

    Make card class locally editable, with a nice dropdown menu

--- tcg_web_editor/assets/js/editor.js
+++ tcg_web_editor/assets/js/editor.js
@@ -1,13 +1,22 @@
-$.tcg_editor = function(context_name) {
+$.tcg_editor = function (context_name) {
+    "use strict";
+    var localStorage,
+        storage_key = "edit_" + context_name,
+        data,
+        storage_listeners = [];
+
     function log() {
-        for (var i = 0; i < arguments.length; i++) {
-            if(window.console) console.log(arguments[i]);
+        var i;
+        if (window.console) {
+            for (i = 0; i < arguments.length; i += 1) {
+                window.console.log(arguments[i]);
+            }
         }
     }
 
     function have_html5_storage() {
         try {
-            return 'localStorage' in window && window['localStorage'] !== null;
+            return window.localStorage !== undefined  && window.localStorage !== null;
         } catch (e) {
             return false;
         }
@@ -15,27 +24,30 @@ $.tcg_editor = function(context_name) {
     if (!have_html5_storage()) {
         return;
     }
+    localStorage = window.localStorage;
 
     // http://stackoverflow.com/questions/4994201/is-object-empty
-    var hasOwnProp = Object.prototype.hasOwnProperty;
     function is_empty(obj) {
-        for (var key in obj) {
-            if (hasOwnProp.call(obj, key))    return false;
+        var key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                return false;
+            }
         }
         return true;
     }
 
-    var storage_key = "edit_" + context_name;
-    var data;
-    var storage_listeners = [];
     function load_from_storage() {
+        var i;
         try {
             data = JSON.parse(localStorage.getItem(storage_key));
-            if (data === null) data = {};
+            if (data === null) {
+                data = {};
+            }
         } catch (e) {
-            data = {}
+            data = {};
         }
-        for (var i = 0; i < storage_listeners.length; i++) {
+        for (i = 0; i < storage_listeners.length; i += 1) {
             storage_listeners[i]();
         }
     }
@@ -44,7 +56,7 @@ $.tcg_editor = function(context_name) {
     function data_save() {
         if (is_empty(data)) {
             localStorage.removeItem(storage_key);
-        }else{
+        } else {
             localStorage.setItem(storage_key, JSON.stringify(data));
         }
         log(storage_key, localStorage.getItem(storage_key));
@@ -56,40 +68,108 @@ $.tcg_editor = function(context_name) {
         }
     }
     function data_remove(key) {
-        if (key in data) {
+        if (data.hasOwnProperty(key)) {
             delete data[key];
             data_save();
         }
     }
 
-    function prepare_str() {
-        var obj = $(this);
-        data_key = obj.attr('data-key');
-        obj.attr('contentEditable', true);
-        obj.addClass('editor-field');
-        var orig_value = obj.text();
-        obj.attr('data-origvalue', obj.text());
-        function update() {
-            if(obj.attr('data-origvalue') == obj.text()) {
+    function prepare(options) {
+        var data_key = options.key,
+            orig_value = options.get();
+        function update(value) {
+            var value = options.get();
+            if (orig_value === value) {
                 data_remove(data_key);
-                obj.removeClass('unsaved');
-            }else{
-                data_set(data_key, obj.text());
-                obj.addClass('unsaved');
+                options.mark_saved();
+            } else {
+                data_set(data_key, value);
+                options.mark_unsaved();
             }
         }
         function update_from_storage() {
-            if (data_key in data) {
-                obj.text(data[data_key]);
-            }else{
-                obj.text(orig_value);
+            if (data.hasOwnProperty(data_key)) {
+                options.set(data[data_key]);
+            } else {
+                options.set(orig_value);
             }
             update();
         }
         update_from_storage();
         storage_listeners.push(update_from_storage);
-        obj.on('focus blur keypress keyup click paste', update);
+        return update;
     }
 
-    $('dd[data-type="str"]').each(prepare_str);
-}
+    $('dd[data-type="str"]').each(function () {
+        var obj = $(this),
+            update;
+        update = prepare({
+            get: function () { return obj.text(); },
+            set: function (value) { return obj.text(value); },
+            mark_saved: function () { obj.removeClass('unsaved'); },
+            mark_unsaved: function () { obj.addClass('unsaved'); },
+            key: obj.attr('data-key')
+        });
+        obj.addClass('editor-field');
+        obj.attr('contentEditable', true);
+        obj.on('focus blur keypress keyup click paste', update);
+    });
+
+    $('dd[data-type="select-one"]').each(function () {
+        var obj = $(this),
+            update,
+            attrs,
+            val,
+            i,
+            options = {},
+            rev_options = {},
+            hide_menu = null;
+        attrs = obj.attr('data-options').split(';');
+        for(i = 0; i < attrs.length; i += 1) {
+            val = attrs[i].split('=');
+            options[val[0]] = val[1];
+            rev_options[val[1]] = val[0];
+        }
+        update = prepare({
+            get: function () { return rev_options[obj.text()]; },
+            set: function (value) { return obj.text(options[value]); },
+            mark_saved: function () { obj.removeClass('unsaved'); },
+            mark_unsaved: function () { obj.addClass('unsaved'); },
+            key: obj.attr('data-key')
+        });
+        obj.addClass('editor-field');
+        obj.on('click', function (event) {
+            var menu,
+                item;
+            obj.addClass('dropdown open');
+            if (hide_menu) {
+                hide_menu();
+            } else {
+                menu = $('<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu"></ul>').appendTo( "body" );
+                for(i = 0; i < attrs.length; i += 1) {
+                    (function (value, text) {
+                        if (text != obj.text()) {
+                            item = $('<li><a></a></li>');
+                            item.find('a').text(text);
+                            item.click(function (event) {
+                                obj.text(text);
+                                update();
+                                hide_menu();
+                                event.stopPropagation();
+                            });
+                            menu.append(item);
+                        }
+                    }).apply(this, attrs[i].split('='));
+                }
+                obj.append(menu);
+                hide_menu = function () {
+                    menu.remove()
+                    $(document).unbind('click', hide_menu);
+                    hide_menu = null;
+                };
+                $(document).bind('click', hide_menu);
+                event.stopPropagation();
+            }
+        });
+    });
+};
--- tcg_web_editor/templates/print.mako
+++ tcg_web_editor/templates/print.mako
@@ -3,6 +3,8 @@
 import json
 
 from markupsafe import Markup
+
+from ptcgdex import tcg_tables
 %>
 
 <%
@@ -35,7 +37,8 @@ flavor = print_.pokemon_flavor
             <dt class="span2">Name</dt>
             <dd class="span4" data-key="name" data-type="str">${card.name}</dd>
             <dt class="span2">Card class</dt>
-            <dd class="span4">${card.class_.name}</dd>
+            <dd class="span4" data-key="class" data-type="select-one"
+                data-options="${';'.join(u'{c.name[0]}={c.name}'.format(c=c) for c in request.db.query(tcg_tables.Class))}">${card.class_.name}</dd>
         </dl>
         % if card.subclasses:
         <dl class="row-fluid">
